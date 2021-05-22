cce949cd4ee638996e8de8a786ece765c5cef88f
miller78130 <miller78130@gmail.com>
2021-05-22 13:54:50 -0500

    added app.py

---
+++ app.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+from flask import Flask, jsonify
+
+
+
+#Bring in dictionaries here
+
+
+# 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+
+# 3. Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return (
+        f"Welcome to My Vacation Weather API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
+    )
+
+
+# 4. Define what to do when a user hits the different routes
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    print("Server received request for 'Precipitation' page...")
+    return jsonify(precip_dict)
+
+
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    print("Server received request for 'Stations' page ...")
+    return jsonify(station_dict)
+
+
+
+
+@app.route("/api/v1.0/tobs")
+def stations():
+    print("Server received request for 'tobs' page ...")
+    return jsonify(tobs_dict)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
